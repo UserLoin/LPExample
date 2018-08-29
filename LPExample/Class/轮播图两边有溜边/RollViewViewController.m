5fc313b87af4d0bb4b1389243443392892fa9ced
luopan <799680236@qq.com>
2018-08-29 16:01:15 +0800

    添加两边有留边并且有缩小动画的轮播

--- LPExample/Class/轮播图两边有溜边/RollViewViewController.m
+++ LPExample/Class/轮播图两边有溜边/RollViewViewController.m
@@ -8,9 +8,13 @@
 
 #import "RollViewViewController.h"
 #import "RollView.h"
+#import "DCCycleScrollView.h"
+#import "ViewController.h"
 
-@interface RollViewViewController ()<RollViewDelegate>
+@interface RollViewViewController ()<RollViewDelegate,DCCycleScrollViewDelegate>
 @property (nonatomic, strong) RollView *rollView;
+@property(nonatomic, strong) DCCycleScrollView *banner;
+
 @end
 
 
@@ -22,12 +26,15 @@
     [self creatPicRollView];
     self.view.backgroundColor = [UIColor whiteColor];
     [self setAutomaticallyAdjustsScrollViewInsets:NO];
-
+}
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.banner adjustWhenControllerViewWillAppera];
 }
 -(void)creatPicRollView{
     
     
-    
+    /*第一种两边带间隙的轮播*/
     self.rollView = [[RollView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 150) withDistanceForScroll:20.0f withGap:8.0f];
     
     /** 全屏宽滑动 视图之间间隙,  将 Distance 设置为 -12.0f */
@@ -35,26 +42,50 @@
     // self.rollView.backgroundColor = [UIColor blackColor];
     
     self.rollView.delegate = self;
-    
     [self.view addSubview:self.rollView];
-    
-    
     NSArray *arr = @[@"11.jpg",
                      @"22.jpg",
                      @"33.jpg"];
-    
     [self.rollView rollView:arr];
+    
+    
+    
+    /*第二种两边带间隙的轮播*/
+    NSArray *bannerArray_H = @[@"11.jpg",
+                               @"22.jpg",
+                               @"33.jpg"];
+    DCCycleScrollView *banner = [DCCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 300, [UIScreen mainScreen].bounds.size.width, 150) shouldInfiniteLoop:YES imageGroups:bannerArray_H];
+    //banner.placeholderImage = [UIImage imageNamed:@"placeholderImage.jpg"];
+    banner.cellPlaceholderImage = [UIImage imageNamed:@"placeholderImage.jpg"];
+    banner.autoScrollTimeInterval = 5;
+    banner.autoScroll = YES;
+    banner.isZoom = YES;
+    banner.imgCornerRadius = 4;
+    //banner.itemWidth = [UIScreen mainScreen].bounds.size.width - 40;
+    banner.itemSpace = 10;
+    banner.betweenGap = 20;
+    banner.delegate = self;
+    banner.isZoom = YES;
+    [self.view addSubview:banner];
+    self.banner = banner;
 }
 
-#pragma mark - 滚动视图协议
+#pragma mark - RollViewDelegate
 -(void)didSelectPicWithIndexPath:(NSInteger)index{
-    
     if (index != -1) {
-        
         NSLog(@"%ld", (long)index);
     }
     
 }
+#pragma mark - DCCycleScrollViewDelegate
+// 点击图片的代理
+-(void)cycleScrollView:(DCCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index {
+    NSLog(@"index = %ld",index);
+    ViewController *vc = [[ViewController alloc]init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
